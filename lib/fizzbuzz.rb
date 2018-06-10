5821b8c42d8d6ab696e681517bc59f7a7e737596
cjsmith7 <cjsmith7@outlook.com>
2018-06-09 23:24:21 -0500

    returns fizz when given 3

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,5 +1,6 @@
 class FizzBuzz
     def self.fizzle_buzzle(num)
+        return 'fizz' if num == 3
         return num
     end
 end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -7,4 +7,7 @@ describe 'fizzbuzz' do
     it 'returns 2 when given 2' do
         expect(FizzBuzz::fizzle_buzzle(2)).to eq(2)
     end
+    it 'returns "fizz" when given 3' do
+        expect(FizzBuzz::fizzle_buzzle(3)).to eq('fizz')
+    end
 end
